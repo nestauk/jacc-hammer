e25ee8f792faa17e425938998930a2df603edace
bishax <alex.bishop@nesta.org.uk>
2020-04-01 14:16:11 +0100

    Revert "Empty commit"
    This reverts commit 6d2f2749fb191e1f139111f679f1d8c755d89676.

--- noxfile.py
+++
@@ -1,9 +0,0 @@
-import nox
-
-
-@nox.session(python=["3.7"])
-def tests(session):
-    """ Example usage: nox -- tests/test_console.py """
-    args = session.posargs or ["--cov"]
-    session.run("poetry", "install", external=True)
-    session.run("pytest", *args)
